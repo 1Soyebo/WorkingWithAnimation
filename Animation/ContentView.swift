16b541fe411b8574030f7d37facbf7aec0e9ce2d
1Soyebo <soyeboibukun@gmail.com>
2021-05-21 14:48:12 +0100

    with damping

--- Animation/ContentView.swift
+++ Animation/ContentView.swift
@@ -8,30 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    @State private var animationAmount: CGFloat = 1
+    @State private var animationAmount = 0.0
 
-        var body: some View {
-            
-            print(animationAmount)
-
-            return VStack {
-                Stepper("Scale amount", value: $animationAmount.animation(
-                    Animation.easeInOut(duration: 0.5)
-                        .repeatCount(5, autoreverses: false)
-                ), in: 1...10)
-
-                Spacer()
-
-                Button("Tap Me") {
-                    self.animationAmount += 1
+    var body: some View {
+            Button("Tap Me") {
+                withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
+                    self.animationAmount += 360
                 }
-                .padding(40)
-                .background(Color.red)
-                .foregroundColor(.white)
-                .clipShape(Circle())
-                .scaleEffect(animationAmount)
             }
+            .padding(50)
+            .background(Color.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
+
         }
 }
 
