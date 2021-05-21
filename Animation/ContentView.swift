47edd7eb45eb0b00d22748d6ad823bdf352931d1
1Soyebo <soyeboibukun@gmail.com>
2021-05-21 14:23:40 +0100

    Initial Commit

---
+++ Animation/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  Animation
+//
+//  Created by Ibukunoluwa Soyebo on 20/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var animationAmount: CGFloat = 1
+
+    
+    var body: some View {
+        Button("Tap Me") {
+            // do nothing
+            self.animationAmount += 0.1
+        }
+        .padding(40)
+        .background(Color.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .overlay(
+            Circle()
+                .stroke(Color.red)
+                .scaleEffect(animationAmount)
+                .opacity(Double(2 - animationAmount))
+                .animation(
+                    Animation.easeOut(duration: 1)
+                        .repeatForever(autoreverses: false)
+                )
+        )
+        .onAppear {
+            self.animationAmount = 2
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
