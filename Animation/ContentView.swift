25dfcfcd2025306ac41beaee3e15c7ce5dca057f
1Soyebo <soyeboibukun@gmail.com>
2021-07-22 19:31:36 +0100

    final update before Paul Hudson says I should go back to update something

--- Animation/ContentView.swift
+++ Animation/ContentView.swift
@@ -7,6 +7,24 @@
 
 import SwiftUI
 
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+
+    func body(content: Content) -> some View {
+        content.rotationEffect(.degrees(amount), anchor: anchor).clipped()
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(
+            active: CornerRotateModifier(amount: 90, anchor: .bottomLeading),
+            identity: CornerRotateModifier(amount: 0, anchor: .topLeading)
+        )
+    }
+}
+
 struct ContentView: View {
     
     @State private var enabled = false
@@ -32,6 +50,7 @@ struct ContentView: View {
                 Rectangle()
                     .fill(Color.red)
                     .frame(width: 200, height: 200)
+                    .transition(.pivot)
             }
             
         }
